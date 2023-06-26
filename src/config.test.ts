bd99aa2ba4129185e5f86634fbccaa62036a3eab
Santiago Aceñolaza <santiago@api3.org>
2023-06-26 11:15:58 -0300

    Add secrets interpolation test

--- src/config.test.ts
+++ src/config.test.ts
@@ -52,4 +52,53 @@ describe('loadConfig', () => {
       loadConfig(configPath);
     }).toThrowError('Secrets interpolation failed');
   });
+
+  it('should interpolate variables in the config using the provided secrets', () => {
+    const configPath = './config/config.json';
+    const merkleFunderDepositories = [
+      {
+        owner: generateRandomAddress(),
+        values: [
+          {
+            recipient: generateRandomAddress(),
+            lowThreshold: { value: 1, unit: 'wei' },
+            highThreshold: { value: 10, unit: 'wei' },
+          },
+        ],
+      },
+    ];
+    const configData = {
+      31337: {
+        rpcUrl: 'https://${RPC_HOST}:${RPC_PORT}',
+        privateKey: '${PRIVATE_KEY}',
+        merkleFunderDepositories,
+      },
+    };
+    const privateKey = generateRandomBytes32();
+    const secrets = {
+      RPC_HOST: 'example.com',
+      RPC_PORT: '8545',
+      PRIVATE_KEY: privateKey,
+    };
+    const expectedInterpolatedConfig = {
+      31337: {
+        rpcUrl: 'https://example.com:8545',
+        privateKey: privateKey,
+        merkleFunderDepositories,
+      },
+    };
+
+    const mockedReadFileSync = jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(JSON.stringify(configData));
+
+    const originalProcessEnv = { ...process.env }; // Backup original process.env
+
+    process.env = { ...secrets }; // Mock process.env with secrets
+
+    const result = loadConfig(configPath);
+
+    expect(mockedReadFileSync).toHaveBeenCalledWith(configPath, 'utf-8');
+    expect(result).toEqual(expectedInterpolatedConfig);
+
+    process.env = originalProcessEnv; // Restore original process.env
+  });
 });
