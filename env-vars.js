8812e4f7e35de299931d3d2c6b0d70334e705f9c
bbenligiray <bbenligiray@gmail.com>
2023-07-10 16:58:53 +0300

    Do not allow undefined dotenv result

--- env-vars.js
+++ env-vars.js
@@ -2,7 +2,7 @@
 const dotenv = require('dotenv');
 
 module.exports = async () => {
-  return Object.entries(dotenv.config({ path: '.env' }).parsed).reduce((acc, [key, value]) => {
+  return Object.entries(dotenv.config({ path: '.env' }).parsed || {}).reduce((acc, [key, value]) => {
     if (!key.startsWith('ETHERSCAN_API_KEY')) {
       return { ...acc, [key]: value };
     }
