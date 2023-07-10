336efc7cb7e3b529808379f9fc3bdb88ce263b7f
bbenligiray <bbenligiray@gmail.com>
2023-07-10 16:40:19 +0300

    Avoid mutating object

--- env-vars.js
+++ env-vars.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 module.exports = async () => {
   return Object.entries(dotenv.config({ path: '.env' }).parsed).reduce((acc, [key, value]) => {
     if (!key.startsWith('ETHERSCAN_API_KEY')) {
-      acc[key] = value;
+      return { ...acc, [key]: value };
     }
     return acc;
   }, {});
