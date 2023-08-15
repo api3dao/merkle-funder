142bf0ea49c2853308d66a609bd1fbb6248a84c1
Santiago Aceñolaza <santiago@api3.org>
2023-08-15 19:00:35 -0300

    Adds tests for validateConfig()

--- src/config.test.ts
+++ src/config.test.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
+import { ZodError } from 'zod';
 import { generateRandomAddress } from '../test/test-utils';
-import { loadConfig } from './config';
+import { loadConfig, validateConfig } from './config';
 
 jest.mock('fs');
 
@@ -166,5 +167,166 @@ describe('loadConfig', () => {
     expect(result).toEqual(expectedInterpolatedConfig);
 
     process.env = originalProcessEnv; // Restore original process.env
+  });
+});
+
+describe('validateConfig', () => {
+  it('should not throw an error if the config is valid', () => {
+    const configData = {
+      31337: {
+        funderMnemonic: '${MNEMONIC}',
+        providers: {
+          ethereum_1: {
+            url: '${FUNDER_RPC_URL_LOCALHOST_1}',
+          },
+        },
+        options: {
+          gasPriceOracle: [
+            {
+              gasPriceStrategy: 'providerRecommendedGasPrice',
+              recommendedGasPriceMultiplier: 1.2,
+            },
+            {
+              gasPriceStrategy: 'constantGasPrice',
+              gasPrice: {
+                value: 10,
+                unit: 'gwei',
+              },
+            },
+          ],
+          fulfillmentGasLimit: 200000,
+        },
+        merkleFunderDepositories: [
+          {
+            owner: generateRandomAddress(),
+            values: [
+              {
+                recipient: generateRandomAddress(),
+                lowThreshold: { value: 1, unit: 'wei' },
+                highThreshold: { value: 10, unit: 'wei' },
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    expect(() => validateConfig(configData, true)).not.toThrow();
+  });
+
+  it('should throw an error if the config has invalid interplation strings', () => {
+    const configData = {
+      31337: {
+        funderMnemonic: '${mnemonic}',
+        providers: {
+          ethereum_1: {
+            url: '${RPC_URL_LOCALHOST}',
+          },
+        },
+        options: {
+          gasPriceOracle: [
+            {
+              gasPriceStrategy: 'providerRecommendedGasPrice',
+              recommendedGasPriceMultiplier: 1.2,
+            },
+            {
+              gasPriceStrategy: 'constantGasPrice',
+              gasPrice: {
+                value: 10,
+                unit: 'gwei',
+              },
+            },
+          ],
+          fulfillmentGasLimit: 200000,
+        },
+        merkleFunderDepositories: [
+          {
+            owner: generateRandomAddress(),
+            values: [
+              {
+                recipient: generateRandomAddress(),
+                lowThreshold: { value: 1, unit: 'wei' },
+                highThreshold: { value: 10, unit: 'wei' },
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    expect(() => validateConfig(configData, true)).toThrowError(
+      new ZodError([
+        {
+          code: 'custom',
+          message: 'Invalid mnemonic',
+          path: ['31337', 'funderMnemonic'],
+        },
+        {
+          validation: 'url',
+          code: 'invalid_string',
+          message: 'Invalid url',
+          path: ['31337', 'providers', 'ethereum_1', 'url'],
+        },
+      ])
+    );
+  });
+
+  it('should throw an error if the config is invalid', () => {
+    const configData = {
+      31337: {
+        funderMnemonic: '${MNEMONIC}',
+        providers: {
+          ethereum_1: {
+            url: '${FUNDER_RPC_URL_LOCALHOST_1}',
+          },
+        },
+        options: {
+          gasPriceOracle: [
+            {
+              gasPriceStrategy: 'providerRecommendedGasPrice',
+              recommendedGasPriceMultiplier: 1.2,
+            },
+            {
+              gasPriceStrategy: 'constantGasPrice',
+              gasPrice: {
+                value: 10,
+                unit: 'gwei',
+              },
+            },
+          ],
+          fulfillmentGasLimit: 200000,
+        },
+        merkleFunderDepositories: [
+          {
+            owner: '0x',
+            values: [
+              {
+                address: generateRandomAddress(),
+                lowThreshold: { value: 1, unit: 'wei' },
+                highThreshold: { value: 10, unit: 'wei' },
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    expect(() => validateConfig(configData, true)).toThrowError(
+      new ZodError([
+        {
+          validation: 'regex',
+          code: 'invalid_string',
+          message: 'Invalid',
+          path: ['31337', 'merkleFunderDepositories', 0, 'owner'],
+        },
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'undefined',
+          path: ['31337', 'merkleFunderDepositories', 0, 'values', 0, 'recipient'],
+          message: 'Required',
+        },
+      ])
+    );
   });
 });
