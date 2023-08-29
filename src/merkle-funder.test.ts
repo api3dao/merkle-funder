71f449e330db76652cb5edea8093286875c810d4
Santiago Aceñolaza <santiago@api3.org>
2023-08-29 12:02:16 -0300

    Adds test to merkle-funder.test to verify custom error names are logged out

--- src/merkle-funder.test.ts
+++ src/merkle-funder.test.ts
@@ -182,7 +182,144 @@ describe('fundChainRecipients', () => {
     expect(loggerInfoSpy).toHaveBeenCalledWith(`Sent tx with hash ${tryMulticallResult.hash}`, logOptions);
   });
 
-  it('should handle failed callStatic.tryMulticall', async () => {
+  it('should handle failed callStatic.tryMulticall and log out the parsed custom error name', async () => {
+    const owner = '0x123456789abcdef';
+    const values = [
+      {
+        recipient: '0xrecipient1',
+        lowThreshold: { value: 10, unit: 'ether' as NamedUnits },
+        highThreshold: { value: 20, unit: 'ether' as NamedUnits },
+      },
+      {
+        recipient: '0xrecipient2',
+        lowThreshold: { value: 5, unit: 'ether' as NamedUnits },
+        highThreshold: { value: 15, unit: 'ether' as NamedUnits },
+      },
+    ];
+    const treeRoot = '0xmerkleroot';
+    const proof1 = ['0xproof1'];
+    const proof2 = ['0xproof2'];
+    const staticMulticallCalldata = ['0xgetBlockNumberCalldata', '0xcalldata1', '0xcalldata2'];
+    const successes = [true, false, true];
+    const functionSelector = '0x08c379a0';
+    const returndata = [
+      ethers.utils.hexlify(10),
+      functionSelector.concat(ethers.utils.defaultAbiCoder.encode(['string'], ['mocked-revert-string']).substring(2)),
+      ethers.utils.hexlify(ethers.utils.toUtf8Bytes('')),
+    ];
+    const tryMulticallResult = {
+      hash: '0xhash',
+    };
+
+    (buildMerkleTree as jest.Mock).mockReturnValueOnce({
+      root: treeRoot,
+      getProof: jest.fn().mockReturnValueOnce(proof1).mockReturnValueOnce(proof2),
+      render: jest.fn().mockReturnValue('mocked-merkle-tree-render'),
+    });
+
+    const encodeFunctionDataMock = jest
+      .fn()
+      .mockReturnValueOnce(staticMulticallCalldata[0])
+      .mockReturnValueOnce(staticMulticallCalldata[1])
+      .mockReturnValueOnce(staticMulticallCalldata[2]);
+
+    const mockContractCallStaticTryMulticall = jest.fn().mockResolvedValueOnce({
+      successes,
+      returndata,
+    });
+
+    const mockContractTryMulticall = jest.fn().mockResolvedValueOnce(tryMulticallResult);
+
+    const mockGetTransactionCount = jest.fn().mockImplementation(() => Promise.resolve(1));
+
+    const mockContract = {
+      interface: {
+        encodeFunctionData: encodeFunctionDataMock,
+        parseError: () => ({ name: 'CustomError' }),
+      },
+      callStatic: {
+        tryMulticall: mockContractCallStaticTryMulticall,
+      },
+      tryMulticall: mockContractTryMulticall,
+      signer: { getTransactionCount: mockGetTransactionCount },
+    };
+
+    const options: ChainOptions = {
+      gasPriceOracle: [
+        {
+          gasPriceStrategy: 'providerRecommendedGasPrice',
+          recommendedGasPriceMultiplier: 1.2,
+        },
+        {
+          gasPriceStrategy: 'constantGasPrice',
+          gasPrice: {
+            value: 10,
+            unit: 'gwei',
+          },
+        },
+      ],
+      fulfillmentGasLimit: 200000,
+    };
+
+    const merkleFunderDepositories: MerkleFunderDepositories = [
+      {
+        owner,
+        values,
+      },
+    ];
+
+    // Capture console outputs
+    const loggerInfoSpy = jest.spyOn(logger, 'info');
+    const loggerErrorSpy = jest.spyOn(logger, 'error');
+
+    await fundChainRecipients(
+      '31337',
+      { options, merkleFunderDepositories },
+      mockContract as unknown as ethers.Contract,
+      logOptions
+    );
+
+    expect(buildMerkleTree).toHaveBeenCalledTimes(1);
+    expect(buildMerkleTree).toHaveBeenCalledWith(values);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Processing 1 merkleFunderDepositories...', logOptions);
+    expect(mockContract.interface.encodeFunctionData).toHaveBeenCalledWith('getBlockNumber()');
+    expect(mockContract.interface.encodeFunctionData).toHaveBeenCalledWith(
+      'fund(address,bytes32,bytes32[],address,uint256,uint256)',
+      [owner, treeRoot, proof1, values[0].recipient, ethers.utils.parseEther('10'), ethers.utils.parseEther('20')]
+    );
+    expect(mockContract.interface.encodeFunctionData).toHaveBeenCalledWith(
+      'fund(address,bytes32,bytes32[],address,uint256,uint256)',
+      [owner, treeRoot, proof2, values[1].recipient, ethers.utils.parseEther('5'), ethers.utils.parseEther('15')]
+    );
+    expect(mockContractCallStaticTryMulticall).toHaveBeenCalledWith(staticMulticallCalldata);
+    expect(loggerInfoSpy).toHaveBeenCalledWith(
+      'Block number fetched while testing funding of recipients: 10',
+      logOptions
+    );
+    expect(loggerErrorSpy).not.toHaveBeenCalledWith(
+      'Failded to fetch block number:',
+      expect.any(String),
+      expect.any(Error),
+      logOptions
+    );
+    expect(loggerInfoSpy).toHaveBeenCalledWith(
+      'Funding test of 0xrecipient1 reverted with message: CustomError',
+      logOptions
+    );
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Funding test of 0xrecipient2 succeeded', logOptions);
+    expect(mockGetTransactionCount).toHaveBeenCalledTimes(1);
+    expect(getGasPriceMock).toHaveBeenCalledTimes(1);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('mocked-get-gas-price-message');
+    expect(mockContractTryMulticall).toHaveBeenCalledWith([staticMulticallCalldata[2]], expect.anything());
+    expect(loggerErrorSpy).not.toHaveBeenCalledWith(
+      expect.stringContaining('Failed to call merkleFunderContract.tryMulticall:'),
+      expect.any(Error),
+      logOptions
+    );
+    expect(loggerInfoSpy).toHaveBeenCalledWith(`Sent tx with hash ${tryMulticallResult.hash}`, logOptions);
+  });
+
+  it('should handle failed callStatic.tryMulticall and log out the decoded revert string', async () => {
     const owner = '0x123456789abcdef';
     const values = [
       {
